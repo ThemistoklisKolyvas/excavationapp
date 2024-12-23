728b3f835b5f36b07a2881966b6bd95cb14dd7ed
Themistoklis Kolyvas <themiskolivas@hotmail.com>
2024-12-23 22:12:36 +0200

    an edit on views

--- excavation/excavation/urls.py
+++ excavation/excavation/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 
 
-from pages.views import home_view, contact_view, about_view
+from pages.views import base_view
 
 urlpatterns = [
     path('' , base_view, name='base'),
